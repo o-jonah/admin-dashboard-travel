b37344946cf348e8fa82ec889889b25e2c5c0279
Jonah Odongo <126646855+o-jonah@users.noreply.github.com>
2025-05-13 23:04:21 +0300

    Add NavItems component and integrate with AdminLayout; include dayjs dependency

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,6 +1,17 @@
+import {SidebarComponent} from '@syncfusion/ej2-react-navigations';
+import { NavItems } from 'components';
+
 const AdminLayout = () => {
   return (
-    <div className='admin-layout'> Admin Layout</div>
+    <div className='admin-layout'> 
+       MobileSidebar
+       <aside className="w-full max-w-[270px] hidden lg:block">
+          <SidebarComponent width={270} enableGestures={false} >
+            <NavItems /> 
+          </SidebarComponent> 
+       </aside>
+
+    </div>
   )
 }
 
